e7e045fd6270431026f924531bf49957fb42588a
Gerald Nash <g.nash.dev@gmail.com>
2018-11-13 13:39:09 -0500

    Add logic to iteratively calculate input length

--- project2.s
+++ project2.s
@@ -45,6 +45,14 @@ input_len:
 	addi $t1, $t1, 10
 	add $t4, $t4, $a0
 
+len_iteration:
+	lb $t2, 0($a0)
+	beqz $t2, after_len_found
+	beq $t2, $t1, after_len_found
+	addi $a0, $a0, 1
+	addi $t0, $t0, 1
+	j len_iteration
+
 exit:
   li $v0, 10
   syscall
