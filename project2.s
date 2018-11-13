7ee95247a392306e1e59d3e898b8eb476089c597
Gerald Nash <g.nash.dev@gmail.com>
2018-11-13 13:53:47 -0500

    Add slot for filling first digit (multiplier: 33 ^ 3)

--- project2.s
+++ project2.s
@@ -120,6 +120,15 @@ serialize_result:
 	beq $s0, $s1, third_digit
 	beq $s0, $s5, fourth_digit
 
+first_digit:
+	li $s6, 35937
+	mult $s4, $s6
+	mflo $s7
+	add $t7, $t7, $s7
+	addi $s0, $s0, -1
+	addi $a0, $a0, 1
+	j base_convert_input
+
 exit:
   li $v0, 10
   syscall
