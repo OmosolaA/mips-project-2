d18fca2e77037336e3dbe5b8ff28a6b98f9f60e3
Gerald Nash <g.nash.dev@gmail.com>
2018-11-13 13:36:16 -0500

    Redo delete left pad logic

--- project2.s
+++ project2.s
@@ -29,43 +29,12 @@ main:
   li $a1, 100
   syscall
 
-  la $t0, userInput
-get_str_len:
-  lb $t1, 0($t0)
-  beq $t1, $zero, after_get_str_len
-  addi $t0, $t0, 1
-  j get_str_len
-after_get_str_len:
-  la $t1, userInput
-  sub $t2, $t0, $t1
-  addi $s0, $t2, -1 # s0 has input len (without null term)
-
-la $a0, userInput
 delete_left_pad:
-  li $t8, 32      #Save space character to t8
-  lb $t9, 0($a0)
-  beq $t8, $t9, remove_first_char
-  move $t9, $a0
-  j print_str_char
-
-remove_first_char:
-  j exit
-  addi $a0, $a0, 1
-  j delete_left_pad
-
-beq $s0, $zero, err_empty_input 
-
-addi $t0, $s0, 0
-print_str_char:
-  la $a1, userInput
-  sub $t1, $s0, $t0
-  addu $a1, $a1, $t1
-  lbu $a0, ($a1) # a0 has character at index t1
-  li $v0, 11
-  syscall
-  sub $t0, $t0, 1
-  beq $t0, $zero, exit
-  j print_str_char
+	li $t8, 32 # space
+	lb $t9, 0($a0)
+	beq $t8, $t9, delete_first_char
+	move $t9, $a0
+	j input_len
 
 exit:
   li $v0, 10
